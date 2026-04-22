694eb4ccb77fcadb24aee5c501383d204cf8fbda
Bryce Tolman <brycealantolman@gmail.com>
2026-04-22 13:59:50 -0500

    adding script

---
+++ ghp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 1. Build the project
+echo "Building..."
+cp -r web/* dist/
+
+# 2. Copy the files into the web branch
+git checkout web
+rm -rf *
+git checkout main -- dist/
+cp -rf dist/* .
+rm -rf dist/
+
+# 3. Commit and push the changes
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push origin web
+
+# 4. Return to main
+git checkout main
