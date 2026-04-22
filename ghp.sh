9e70df907cd08d423409e3ab475d5d3d2b3f6cb9
Bryce Tolman <brycealantolman@gmail.com>
2026-04-22 14:41:34 -0500

    testing:

--- ghp.sh
+++ ghp.sh
@@ -6,17 +6,20 @@ cp -r web/* dist/
 
 # 2. Copy the files into the web branch
 git stash > /dev/null
-git checkout web
+git checkout web > /dev/null
 
-rm -rf *.html *.js *.css
+rm -f *.html *.js *.css 2> /dev/null
 cp -rf dist/* .
 
 # 3. Commit and push the changes
-git add *.html *.js *.css
+git add *.html 2> /dev/null
+git add *.js 2> /dev/null
+git add *.css 2> /dev/null
+
 git commit -am "Deploy to GitHub Pages"
 git push origin web
 
 # 4. Return to main
-git checkout main
+git checkout main > /dev/null
 git stash pop > /dev/null
 
