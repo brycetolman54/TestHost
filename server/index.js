c0439a4ef41cff4560a099d1f1bd561ca125a4a9
Bryce Tolman <brycealantolman@gmail.com>
2026-04-22 17:31:00 -0500

    Deploying Vercel

--- server/index.js
+++ server/index.js
@@ -1,3 +1,19 @@
-export default function handler(req, res) {
-    res.status(200).json("Hello");
-}
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.set('trust proxy', true);
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
