485371ab4346868497eae414001f7305e483a042
Bryce Tolman <brycealantolman@gmail.com>
2026-04-22 18:59:18 -0500

    Deploying GitHub Pages

--- web/app.js
+++ web/app.js
@@ -1,5 +1,5 @@
 async function ping() {
-    const res = await fetch("test-host-roan.vercel.app/api/hello");
+    const res = await fetch("https://test-host-roan.vercel.app/api/hello");
     const data = await res.json();
     document.getElementById("out").innerText = data.message;
 }
