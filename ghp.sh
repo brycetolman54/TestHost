361b7c348913e6acdbe0137e107ebfcfe980bd3a
Bryce Tolman <brycealantolman@gmail.com>
2026-04-22 14:15:03 -0500

    Deploy to GitHub Pages

--- ghp.sh
+++ ghp.sh
@@ -3,18 +3,24 @@
 # 1. Build the project
 echo "Building..."
 cp -r web/* dist/
-sleep 5
+
+branch=$(git branch --show-current)
+if [ "$branch" != "main" ]; then
+    echo "On Web"
+else
+    echo "not on web"
+fi
 
 # 2. Copy the files into the web branch
 git checkout web
-rm -rf *
+rm -rf *.html *.js *.css
 git checkout main -- dist/
 cp -rf dist/* .
 rm -rf dist/
 
 # 3. Commit and push the changes
 git add .
-git commit -m "Deploy to GitHub Pages"
+git commit -am "Deploy to GitHub Pages"
 git push origin web
 
 # 4. Return to main
