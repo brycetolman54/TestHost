2dacfe4b26a760afbfc3d768cc66191bb66b1b85
Bryce Tolman <brycealantolman@gmail.com>
2026-04-22 14:16:40 -0500

    Deploy to GitHub Pages

--- app.js
+++
@@ -1,5 +0,0 @@
-async function ping() {
-  const res = await fetch("https://your-backend.vercel.app/api");
-  const data = await res.json();
-  document.getElementById("out").innerText = data.message;
-}
