8d515e4bbd7bbb97474f8886e1e32e8592db1cb2
Bryce Tolman <brycealantolman@gmail.com>
2026-04-22 17:13:05 -0500

    update

--- api/index.js
+++ api/index.js
@@ -1,3 +1,13 @@
-export default function handler(req, res) {
-    res.status(200).json("Hello");
-}
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
