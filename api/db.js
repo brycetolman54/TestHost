7807ebccc44ab3be8647667b2063f422e1aef1c1
Bryce Tolman <brycealantolman@gmail.com>
2026-04-22 18:32:49 -0500

    update

--- api/db.js
+++
@@ -1,3 +0,0 @@
-export default const db = {
-  message: "Hello there!",
-};
--- api/index.js
+++
@@ -1,5 +0,0 @@
-// import db from "./db.js";
-
-export default function handler(req, res) {
-  res.status(200).send("hello");
-}
